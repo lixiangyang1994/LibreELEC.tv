c3b8674e63ca952921f33b497087441b07ef90d2
Christian Hewitt <christianshewitt@gmail.com>
2021-04-07 16:51:55 +0000

    u-boot: bump to 2021.04

--- packages/tools/u-boot/package.mk
+++ packages/tools/u-boot/package.mk
@@ -35,8 +35,8 @@ case "${PROJECT}" in
     PKG_PATCH_DIRS="rockchip"
     ;;
   *)
-    PKG_VERSION="2021.01"
-    PKG_SHA256="b407e1510a74e863b8b5cb42a24625344f0e0c2fc7582d8c866bd899367d0454"
+    PKG_VERSION="2021.04"
+    PKG_SHA256="0d438b1bb5cceb57a18ea2de4a0d51f7be5b05b98717df05938636e0aadfe11a"
     PKG_URL="http://ftp.denx.de/pub/u-boot/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
     PKG_PATCH_DIRS="default"
     ;;
